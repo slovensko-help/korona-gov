1af534da8e970d260c9c6a5683588d07e7fdec6d
Peter Bohus <peter.bohus@gmail.com>
2020-04-09 13:44:05 +0200

    vytvorenie metaboxov pre späť a auto generovanie title v template

--- wp-content/themes/korona/page.php
+++ wp-content/themes/korona/page.php
@@ -1,15 +1,25 @@
 <?php
 
-get_header();
+    get_header();
 
-if ( have_posts() ) {
+    $back_text = get_post_meta( get_the_ID(), 'gov_back_button_text', TRUE );
+    $back_link = get_post_meta( get_the_ID(), 'gov_back_button_url', TRUE );
 
-    // Load posts loop.
-    while ( have_posts() ) {
-        the_post();
-        //remove_filter('the_content', 'wpautop', 12);
-        the_content();
+    if ( !empty( $back_link ) ) :
+        echo '<a href="' . esc_url( $back_link ) . '" class="govuk-back-link">' . esc_html( $back_text ) . '</a>';
+    endif;
+
+    echo '<h1 class="govuk-heading-xl govuk-!-margin-bottom-6">' . get_the_title() . '</h1>';
+
+    if ( have_posts() ) {
+
+        // Load posts loop.
+        while ( have_posts() ) {
+            the_post();
+            //remove_filter('the_content', 'wpautop', 12);
+            the_content();
+        }
     }
-}
 
-get_footer();
+
+    get_footer();
