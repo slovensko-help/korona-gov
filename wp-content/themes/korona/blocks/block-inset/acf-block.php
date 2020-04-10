1f437842257ae3a929d171c8a84aed009b4e4485
Majko Hucik <huco@MacBook-Pro-uzivatela-Majko.local>
2020-04-10 10:02:45 +0200

    update acf

--- wp-content/themes/korona/blocks/block-inset/acf-block.php
+++ wp-content/themes/korona/blocks/block-inset/acf-block.php
@@ -1,3 +1,51 @@
 <?php
+if (!defined('ABSPATH')) {
+    exit;
+}
 
+if( function_exists('acf_add_local_field_group') ):
 
+    acf_add_local_field_group(array(
+        'key' => 'group_5e8f6e96c7ee9',
+        'title' => 'Vsadený text',
+        'fields' => array(
+            array(
+                'key' => 'field_5e8f6ea12db03',
+                'label' => 'Text',
+                'name' => 'text',
+                'type' => 'text',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+                'prepend' => '',
+                'append' => '',
+                'maxlength' => '',
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'block',
+                    'operator' => '==',
+                    'value' => 'acf/section-inset',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+    ));
+
+endif;
