47920e19a3f2a0024cafaae0a3725f6b25671ef9
“Filip <filip@bratia.sk>
2020-04-19 21:14:45 +0200

    Implement static drive-ins generator

--- tools/update-content/all.php
+++ tools/update-content/all.php
@@ -2,6 +2,6 @@
 
 define('ROOT_DIR', __DIR__ . '');
 
-//include_once 'homepage/all.php';
-//include_once 'faq/all.php';
+include_once 'homepage/all.php';
+include_once 'faq/all.php';
 include_once 'driveins/all.php';
