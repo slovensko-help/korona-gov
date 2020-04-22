f1b1a5a6c916d66b3f9604354f9b2263d5d95a5b
jurajk <kiss@lbstudio.sk>
2020-04-22 19:59:38 +0200

    move to a separate file

--- wp-content/themes/korona/functions.php
+++ wp-content/themes/korona/functions.php
@@ -1,5 +1,6 @@
 <?php
 include 'inc/frontend.php';
+include 'inc/block-editor-adjustments.php';
 
     //add_action('get_header', 'korona_filter_head');
     //
@@ -262,18 +263,3 @@ function korona_taxonomy() {
 
     }
     add_action( 'init', 'korona_taxonomy', 0 );
-
-// Disable support for custom text color in WYSIWYG editor
-function korona_remove_text_color_from_mce( $buttons ) {
-    array_splice($buttons, array_search('forecolor', $buttons ), 1);
-    return $buttons;
-}
-
-add_filter( 'mce_buttons_2', 'korona_remove_text_color_from_mce' );
-
-function korona_gutenberg_disable_all_colors() {
-    add_theme_support( 'editor-color-palette', array() );
-    add_theme_support( 'disable-custom-colors' );
-}
-
-add_action( 'after_setup_theme', 'korona_gutenberg_disable_all_colors' );
