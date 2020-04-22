5fec6182ca03c4ed2b8c35f6356b3d2477eeb1ba
jurajk <kiss@lbstudio.sk>
2020-04-22 19:58:07 +0200

    disable block editor custom colors and palette

--- wp-content/themes/korona/functions.php
+++ wp-content/themes/korona/functions.php
@@ -270,3 +270,10 @@ function korona_remove_text_color_from_mce( $buttons ) {
 }
 
 add_filter( 'mce_buttons_2', 'korona_remove_text_color_from_mce' );
+
+function korona_gutenberg_disable_all_colors() {
+    add_theme_support( 'editor-color-palette', array() );
+    add_theme_support( 'disable-custom-colors' );
+}
+
+add_action( 'after_setup_theme', 'korona_gutenberg_disable_all_colors' );
