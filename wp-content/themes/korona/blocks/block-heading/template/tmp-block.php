07093f53fb703cca925879fd4995262b2bc714d8
Peter Bohus <peter.bohus@gmail.com>
2020-05-14 13:34:23 +0200

    zrušenie auto-generovania

--- wp-content/themes/korona/blocks/block-heading/template/tmp-block.php
+++ wp-content/themes/korona/blocks/block-heading/template/tmp-block.php
@@ -5,17 +5,15 @@ if (!defined('ABSPATH')) {
 
 $block_id = $block['id'];
 $block_name = $block['title'];
+$id_nadpisu='';
 
 if ($nadpis = get_field('nadpis')) {
 
     $id_nadpisu = get_field( 'id_nadpisu' );
 
-    if( empty( $id_nadpisu ) ) {
-        $id_nadpisu = $nadpis;
-    }
     ?>
 
-    <<?= get_field('uroven_nadpisu')?> id="<?= sanitize_title( $id_nadpisu ) ?>" class="<?= get_field('velkost_nadpisu')?>"><?= wp_kses( $nadpis, [ 'br' => [] ] ) ?></<?= get_field('uroven_nadpisu')?>>
+    <<?= get_field('uroven_nadpisu')?><?= ( !empty( $id_nadpisu ) ? ' id="' . sanitize_title( $id_nadpisu ) . '"' : '' ); ?> class="<?= get_field('velkost_nadpisu')?>"><?= wp_kses( $nadpis, [ 'br' => [] ] ) ?></<?= get_field('uroven_nadpisu')?>>
 
 <?php } else { ?>
 
