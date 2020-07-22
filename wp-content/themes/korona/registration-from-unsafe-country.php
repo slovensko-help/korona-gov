a1ce46c79f88dc273ad5bfd4483d242ad82cbac0
“Filip <filip@bratia.sk>
2020-07-22 12:04:33 +0200

    Bump ehranica js version

--- wp-content/themes/korona/registration-from-unsafe-country.php
+++ wp-content/themes/korona/registration-from-unsafe-country.php
@@ -1,4 +1,4 @@
-<?php define('RC_ASSETS_VERSION', '1.0.8'); ?>
+<?php define('RC_ASSETS_VERSION', '1.0.9'); ?>
 
     <script src="/wp-content/themes/korona/assets/js/moment.min.js?v=<?php echo RC_ASSETS_VERSION; ?>"></script>
     <script src="/wp-content/themes/korona/assets/js/moment-timezone-with-data.js?v=<?php echo RC_ASSETS_VERSION; ?>"></script>
