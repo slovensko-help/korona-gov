ca30de58ef6a4a859ad7afe71ae5788152947fc7
“Filip <filip@bratia.sk>
2020-08-25 14:38:20 +0200

    Bump ehranica js version

--- wp-content/themes/korona/registration-from-unsafe-country.php
+++ wp-content/themes/korona/registration-from-unsafe-country.php
@@ -1,4 +1,4 @@
-<?php define('RC_ASSETS_VERSION', '1.0.12'); ?>
+<?php define('RC_ASSETS_VERSION', '1.0.13'); ?>
     <script>
         var RC_LANGUAGE = '<?php echo apply_filters('wpml_current_language', NULL); ?>';
         var RC_TRANSLATIONS = {
